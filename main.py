1e778110a1a0845e71101cdbccb739fc0572b027
Seth Williams <swilliams9334@gmail.com>
2021-06-22 11:03:55 -0400

    setup some basic paths

--- main.py
+++ main.py
@@ -6,9 +6,30 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': {'name': 'Seth'}}
+    return {'data': 'blog list'}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+# This one _has_ to be above app.get('/blog/{id}) because it reads down the page, so 
+# /blog/unpublished will match the id, but won't match it being an integer
+# so it will throw an error
+@app.get('/blog/unpublished')
+def unpublished():
+    # fetch a list of all unpublished blogs
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    # fetch comments of blog with id = id
+    my_list = []
+    for i in range(1, id + 1):
+        my_list.append({'comment_number': i})
+    print(my_list)
+
+    return {'data': my_list}
