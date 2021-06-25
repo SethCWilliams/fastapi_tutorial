ad382de5bdd2643bd3f70c419b7a0f45d4e94664
Seth Williams <swilliams9334@gmail.com>
2021-06-25 16:07:14 -0400

    made update endpoint

--- blog/main.py
+++ blog/main.py
@@ -34,6 +34,15 @@ def destroy(id, db: Session = Depends(get_db)):
     return {'done'}
 
 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not available')
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
 
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
